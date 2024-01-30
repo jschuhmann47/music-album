878fb5e2c6bd6e308e152c6eebb6a103e331f5a6
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-01-30 17:49:02 -0300

    db query lets go

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ pub mod models;
 pub mod schema;
 
 
+use std::vec;
+
 use axum::{http::StatusCode, routing::get, Json, Router};
+use diesel::{query_dsl::methods::{LimitDsl, SelectDsl}, RunQueryDsl, SelectableHelper};
 use models::Album;
 
 #[tokio::main]
@@ -24,14 +27,23 @@ async fn handler() -> (StatusCode, Json<Album>){
     (StatusCode::OK, Json(Album { id: 123, title: String::from("value"), artist: String::from("value"), cover: String::from("value"), year: 1234 }))
 }
 
-async fn db_handler() -> (StatusCode, Json<Album>){
-    // let db = &mut config::connect_to_db();
+async fn db_handler() -> (StatusCode, Json<Vec<Album>>){
+    use self::schema::albums::dsl::*;
+    let db = &mut config::connect_to_db();
 
+    let results = albums.limit(5).select(Album::as_select()).load(db);
+
+    match results {
+        Ok(res) => (StatusCode::OK, Json(res)),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(vec![]))
+    }
     
-    (StatusCode::OK, Json(Album { id: 123, title: String::from("value"), artist: String::from("value"), cover: String::from("value"), year: 1234 }))
+    // (StatusCode::OK, Json(Album { id: 123, title: String::from("value"), artist: String::from("value"), cover: String::from("value"), year: 1234 }))
 }
 
 fn example_routes() -> Router {
-    let router = Router::new().route("/", get(handler));
+    let router = Router::new()
+    .route("/", get(handler))
+    .route("/db", get(db_handler));
     router
 }

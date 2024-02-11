221b2916d37f0efe63865d36f984e876ea49339a
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-02-11 08:00:16 -0300

    jwt wip

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,24 +1,30 @@
 
-use base16ct::Error;
-use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey, errors::Error as JWTError, TokenData};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey, errors::Error as JWTError, TokenData};
 use serde::{Deserialize, Serialize};
 
 
+// https://github.com/Keats/jsonwebtoken/blob/master/examples/validation.rs
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
-    sub: String, // subject, can use user id
-    exp: usize, // unix expiration date
+    sub: u32, // subject, can use user id
+    exp: u64, // unix expiration date
 }
 
-fn new_claim() -> Claims {
-    Claims{sub: String::from("1"), exp: 1739197106}
+fn new_claim(user_id: u32) -> Claims {
+    Claims{sub: 1, exp: time_now()}
 }
 
 fn generate_token() -> Result<String, JWTError>{
-    encode(&Header::default(), &new_claim(), &EncodingKey::from_secret("secret".as_ref()))
+    encode(&Header::default(), &new_claim(1), &EncodingKey::from_secret("secret".as_ref()))
 }
 
 fn decode_token(token: String) -> Result<TokenData<Claims>, JWTError> {
     decode::<Claims>(&token, &DecodingKey::from_secret("secret".as_ref()), &Validation::default())
+}
+
+fn time_now() -> u64 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
 }

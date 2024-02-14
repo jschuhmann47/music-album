8d1aa1319b87e15c9643fb3f7df20f99f758e1fa
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-02-14 18:41:26 -0300

    todo notes

--- migrations/2024-02-10-122553_create_users/up.sql
+++ migrations/2024-02-10-122553_create_users/up.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (username, password)
-VALUES ('test', '123');
+VALUES ('test', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');

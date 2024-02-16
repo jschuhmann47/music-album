561bb1c72bc6c2ec114cefcbe6a4857478fab9e2
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-02-16 18:10:48 -0300

    add sql columns

--- migrations/2024-01-30-200949_create_albums/up.sql
+++ migrations/2024-01-30-200949_create_albums/up.sql
@@ -1,11 +1,16 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS albums (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
     title VARCHAR(255) NOT NULL,
     artist  VARCHAR(255) NOT NULL,
     cover  VARCHAR(255) NOT NULL,
-    `year` INTEGER NOT NULL
+    `year` INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO albums (title, artist, cover, year)
-VALUES ('Anchu band', 'Bring me the moon', 'https://www.slugmag.com/wp/wp-content/uploads/2013/05/bring-me-the-horizon-sempiternal.webp', 2020);
+INSERT INTO albums (created_at, title, artist, cover, year)
+VALUES (NOW() ,'Anchu band', 'Bring me the moon', 'https://www.slugmag.com/wp/wp-content/uploads/2013/05/bring-me-the-horizon-sempiternal.webp', 2020);
